848c3f0fd9cb81df064aa8f8e7c498acbdda0afe
lucassant <lucasfdesantana095@gmail.com>
2026-08-10 12:42:00 -0300

    refactor: secao de logos

---
+++ destinize/src/components/LogoSection.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="w-full py-2 h-screen flex flex-col items-center justify-evenly 
+    md:flex-row md:h-44
+    ">
+        <img src="../img/latam.png" class="w-44"/>
+        <img src="../img/expedia.png" class="w-44"/>
+        <img src="../img/qantas.png" class="w-44 mt-10 md:ml-10 md:mt-0"/>
+        <img src="../img/alitalia.png" class="w-44 mt-10 md:ml-10 md:mt-0"/>
+    </div>
+</template>
+<script>
+
+</script>
